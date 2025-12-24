f8a3441a2085a5cff9e0ef2f4d15a737918ac925
Bella5768 <bell@csig.edu.gn>
2025-12-24 15:23:40 +0000

    Redeploy: Forcer redéploiement Vercel avec variables d'environnement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// Importer la configuration API en premier pour configurer axios
+import './config/api'
 import App from './App.jsx'
 import './index.css'
 
